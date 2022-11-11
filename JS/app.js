17b17a501e6c5dc831ebd76e552b2688930e688d
AleksandrLozovoy <lozovoialeksandr2019@gmail.com>
2022-11-11 13:43:00 +0300

    added filter for portfolio section

--- JS/app.js
+++ JS/app.js
@@ -1,6 +1,20 @@
 $(function () {
   let filter = $("[data-filter]");
-  filter.on("click", function () {
-    console.log(1);
+  filter.on("click", function (event) {
+    event.preventDefault();
+    let category = $(this).data("filter");
+
+    if (category == "all") {
+      $("[data-category]").removeClass("hide");
+    } else {
+      $("[data-category]").each(function () {
+        let work = $(this).data("category");
+        if (work != category || work == "all") {
+          $(this).addClass("hide");
+        } else {
+          $(this).removeClass("hide");
+        }
+      });
+    }
   });
 });

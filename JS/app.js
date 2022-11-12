842f2e2cfd12583b47ec4dac4a16dad2c995c46d
AleksandrLozovoy <lozovoialeksandr2019@gmail.com>
2022-11-12 16:47:00 +0300

    added open/close modals (jquery)

--- JS/app.js
+++ JS/app.js
@@ -1,4 +1,5 @@
 $(function () {
+  // FILTER
   let filter = $("[data-filter]");
   filter.on("click", function (event) {
     event.preventDefault();
@@ -9,12 +10,49 @@ $(function () {
     } else {
       $("[data-category]").each(function () {
         let work = $(this).data("category");
-        if (work != category || work == "all") {
+        if (work != category) {
           $(this).addClass("hide");
         } else {
           $(this).removeClass("hide");
         }
       });
     }
+  });
+
+  // MODAL
+
+  let modalClick = $("[data-modal]");
+
+  modalClick.on("click", function (event) {
+    event.preventDefault();
+    $this = $(this);
+    modalID = $this.data("modal");
+    $(modalID).addClass("show");
+    $("body").addClass("no-scroll");
+
+    setTimeout(function () {
+      $(modalID).find(".modal__dialog").css({
+        transform: "rotateX(0)",
+      });
+    }, 200);
+  });
+
+  let modalClose = $("[data-close]");
+
+  modalClose.on("click", function (event) {
+    event.preventDefault();
+
+    $(modalID).removeClass("show");
+    $("body").removeClass("no-scroll");
+  });
+
+  $(".modal").on("click", function () {
+    let $this = $(this);
+    $this.removeClass("show");
+    $("body").removeClass("no-scroll");
+  });
+
+  $(".modal__dialog").on("click", function (event) {
+    event.stopPropagation();
   });
 });
